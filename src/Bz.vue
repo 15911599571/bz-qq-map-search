70d10522367bff7ae41c492abcd25c268c2e7fd5
bigzhu <vermiliondun@gmail.com>
2016-07-08 15:29:30 +0800

    init

---
+++ src/Bz.vue
@@ -0,0 +1,107 @@
+<style lang="less">
+</style>
+
+<template>
+  <div class="search">
+    <we-search-bar :search="search" :focus="clearOverlays" cancel="clearOverlays"></we-search-bar>
+    <div v-show="markers.length>1" style='width: 100%; height: 300px' id="infoDiv"></div>
+  </div>
+</template>
+
+<script>
+  import WeSearchBar from 'bz-weui-search-bar'
+  export default {
+    watch: {
+      'map': 'initSearch'
+    },
+    props: {
+      location: {
+        type: String,
+        default: '云南'
+      },
+      map: {
+        required: true
+      }
+    },
+    components: {
+      WeSearchBar
+    },
+    data: function () {
+      return {
+        keyword: '',
+        markers: []
+      }
+    },
+    ready () {
+      // this.initSearch()
+    },
+    computed: {
+    },
+    methods: {
+      initSearch: function () {
+        var v = this
+        this.search_service = new window.qq.maps.SearchService(
+          {
+            map: v.map,
+            // 设置搜索范围
+            location: v.location,
+            // 设置搜索页码为1
+            pageIndex: 0,
+            // 设置每页的结果数为5
+            pageCapacity: 4,
+            // 设置展现查询结构到infoDIV上
+            panel: document.getElementById('infoDiv'),
+            // 设置动扩大检索区域。默认值true，会自动检索指定城市以外区域。
+            autoExtend: true,
+            // 检索成功的回调函数
+            complete: function (results) {
+              // 设置回调函数参数
+              // console.log(results.detail)
+              var pois = results.detail.pois
+              var latlngBounds = new window.qq.maps.LatLngBounds()
+              for (var i in pois) {
+                var poi = pois[i]
+                // 扩展边界范围，用来包含搜索到的Poi点
+                latlngBounds.extend(poi.latLng)
+                var marker = new window.qq.maps.Marker(
+                  {
+                    map: v.map,
+                    position: poi.latLng
+                  }
+                )
+                marker.setTitle(i + 1)
+                v.markers.push(marker)
+              }
+              if (pois) {
+                v.map.fitBounds(latlngBounds)
+              } else {
+                window.alert('未找到' + v.keyword)
+              }
+            },
+            // 若服务请求失败，则运行以下函数
+            error: function (e) {
+              console.log(e)
+              window.alert('出错了。')
+            }
+          }
+        )
+      },
+      search: function (keyword) {
+        this.keyword = keyword
+        this.clearOverlays()
+        if (keyword === '') {
+          return
+        }
+        this.search_service.search(keyword)
+      },
+      clearOverlays: function () {
+        var overlays = this.markers
+        var overlay = overlays.pop()
+        while (overlay) {
+          overlay.setMap(null)
+          overlay = overlays.pop()
+        }
+      }
+    }
+  }
+</script>
---
+++ src/Doc.vue
@@ -0,0 +1,93 @@
+<style lang=less>
+  .original-text-bz {
+    /*保留空格*/
+    white-space: pre-wrap;
+    /*字体能自动换行*/
+    word-wrap:break-word;
+  }
+  code {
+    padding: 2px 4px;
+    font-size: 90%;
+    color: #c7254e;
+    background-color: #f9f2f4;
+    border-radius: 4px;
+    .original-text-bz;
+  }
+  .map {
+    width: 100%;
+    height: 500px;
+  }
+</style>
+
+<template>
+  <div class="ui container segment">
+    <h1>{{name}}</h1>
+    <p>{{desc}}</p>
+    <table class="ui celled table">
+      <thead>
+        <tr><th>参数</th><th>说明</th></tr>
+      </thead>
+      <tbody>
+        <tr v-show="parm_desc">
+          <td colspan="2" v-html="parm_desc"></td>
+        </tr>
+        <tr v-for="parm in parms"> <td class="single line"> {{parm.parm}} </td> <td> {{parm.desc}} </td></tr>
+        <tr v-show="parms.length===0">
+          <td colspan="2"></td>
+        </tr>
+      </tbody>
+    </table>
+    <code v-text="code"></code>
+    <div class="ui divider"></div>
+    <map-search :map="map" ></map-search>
+    <q-map :map.sync="map" class="map"></q-map>
+  </div>
+</template>
+
+<script>
+  import 'semantic-ui/dist/components/site.css'
+  import 'semantic-ui/dist/components/reset.css'
+  import 'semantic-ui/dist/components/table.css'
+  import 'semantic-ui/dist/components/container.css'
+  import 'semantic-ui/dist/components/divider.css'
+
+  import 'weui/dist/style/weui.css'
+
+  import MapSearch from './Bz.vue'
+  import QMap from 'bz-qq-map'
+  export default {
+    components: {
+      QMap,
+      MapSearch
+    },
+    data: function () {
+      return {
+        name: 'bz-oauth2-button',
+        desc: '生成oauth2 button',
+        parm_desc: ` 依赖 semantic css
+        <code>
+        import 'semantic-ui/dist/components/button.css'
+        import 'semantic-ui/dist/components/icon.css'</code>
+        `,
+        oauths: [
+          'vk',
+          'douban',
+          'youtube',
+          'facebook',
+          'google plus',
+          'twitter',
+          'github',
+          'instagram'
+        ],
+        parms: [
+          {parm: 'oauths', desc: '一个list, 指定要显示的oauth方式:' + `['vk', 'twitter']`}
+        ],
+        code: '<oauth2 :oauths="oauths"></oauth2>',
+        map: {}
+      }
+    },
+    methods: {
+    }
+  }
+</script>
+
